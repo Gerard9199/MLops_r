d966b7b4bfaa509a8fbf0db14d080597148cf2c1
Gerardo Alvarez <70041405+Gerard9199@users.noreply.github.com>
2023-03-24 23:09:15 -0600

    Update 02_ingestar.py

--- 02_ingestar.py
+++ 02_ingestar.py
@@ -1,33 +1,14 @@
-#carga
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-def save_data(set_name,dataset):
-	dataset.to_csv(set_name + ".csv",index=False)
-	print("Dataset "+ set_name +".csv listo ...")
-
-def fix_data(A,B):
-  ds= pd.DataFrame()
-  ds= A
-  ds['test_result']=B
-  return ds
-
-def split_data(dataframe_name, validation=False, ss=0.1):
-  datasets= pd.DataFrame()
-  df = pd.read_csv(dataframe_name)
-  X = df.drop('test_result',axis=1) 
-  y = df['test_result']
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ss, random_state=101)
-  if validation == False: 
-    setname_01='train'
-    setname_02='test'
-  else :
-    setname_01='train_01'
-    setname_02='train_02'
-  datasets=fix_data(X_train,y_train)
-  save_data(setname_01,datasets)
-  datasets=fix_data(X_test,y_test)
-  save_data(setname_02,datasets)
-
-split_data('quakes.csv', validation = False, ss = 0.2)
-split_data('train.csv', validation = True, ss = 0.1)
+quakes=pd.read_csv("quakes.csv")
+X=quakes[['lat','long','depth','stations']]
+y=quakes['mag']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+X_train_2,X_validate,y_train_2,y_validate=train_test_split(X_train,y_train,test_size=0.1)
+df_train= X_train_2[['lat','long','depth','stations']].copy()
+df_train['mag'] = y_train_2.copy()
+df_train.to_csv("train.csv",index=False,columns=['lat','long','depth','stations','mag'])
+df_validate= X_validate[['lat','long','depth','stations']].copy()
+df_validate['mag'] = y_validate.copy()
+df_validate.to_csv("validate.csv",index=False,columns=['lat','long','depth','stations','mag'])
+df_test= X_test[['lat','long','depth','stations']].copy()
+df_test['mag'] = y_test
+df_test.to_csv("test.csv",index=False,columns=['lat','long','depth','stations','mag'])
