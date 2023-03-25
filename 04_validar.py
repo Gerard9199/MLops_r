3ba8f5de4de323f48fa7e97a31bc04aba564cb12
Gerardo Alvarez <70041405+Gerard9199@users.noreply.github.com>
2023-03-24 22:58:01 -0600

    Update 04_validar.py

--- 04_validar.py
+++ 04_validar.py
@@ -1,28 +1,30 @@
-import pandas as pd
 import pickle
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+from sklearn.linear_model import LinearRegression
+import pandas as pd
 
-def data_metrics(yP, yL):
-    print(accuracy_score(yP,yL))
-    print(confusion_matrix(yP,yL))
-    print(classification_report(yP,yL))
 
-def split_data(dataframe_name):
-    df = pd.read_csv(dataframe_name)
-    X_01 = df.drop('test_result',axis=1) 
-    y_01 = df['test_result']
-    return (X_01,y_01)
+def function_testing(train, test, alpha, w, iteraciones=10):
+    from math import isclose
+    
+    for i in range(iteraciones):
+        #y=m+x_1*w_1+x_2*w_2+...+x_n*w_n
+        a = (alpha+train.iloc[i].lat*w[0]+train.iloc[i].long*w[1] + train.iloc[i].depth*w[2]+train.iloc[i].stations*w[3])
+        b = test.iloc[i]
+        if isclose(a, b, abs_tol=0.4) == True:
+            print('La predicción tiende al resultado original')
+        else:
+            print('La predicción difiere por más de 0.4 decimales')
 
-def data_prediction(log_model, X_01):
-    y_01 = log_model.predict(X_01)
-    return y_01
+ml_param_validate=pickle.load(open("mlparams", 'rb'))
+df_quakes_test=pd.read_csv('test.csv')
+X_test=df_quakes_test[['lat','long','depth','stations']]
+y_test=df_quakes_test['mag']
+reg_test=ml_param_validate.fit(X_test,y_test)
 
-def test(test_name, filename='mlparams'):
-    log_test = pickle.load(open(filename, 'rb'))
-    (X,y)=split_data(test_name)
-    y_pred = data_prediction(log_test, X)
-    print('Metricas Validación')
-    data_metrics (y_pred,y)
+w = reg_test.coef_
+a = reg_test.intercept_
 
-test('test.csv')
+function_testing(train=X_test, test=y_test, alpha=a, w=w, iteraciones=10)
+
+R_2_test=reg_test.score(X_test,y_test)
+print(R_2_test)
