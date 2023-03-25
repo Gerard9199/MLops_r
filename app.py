9d18e78cc6d3ddd4e1e20a3fdb9d55f1fbbc619e
Gerardo Alvarez <70041405+Gerard9199@users.noreply.github.com>
2023-03-25 00:38:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import request
 import pickle
 import logging
 import sys
-def predict(X_in, w, b):
-    y = b + X_in[0]*w[0]+ X_in[1]*w[1] + X_in[2]*w[2] + X_in[3]*w[3]
+def predict(x_1,x_2,x_3,x_4,w,b):
+    y = b + x_1*w[0]+ x_2*w[1] + x_3*w[2] + x_4*w[3]
     return y
 
 print(__name__)
@@ -14,15 +14,20 @@ api_mlparams=pickle.load(open("mlparams", 'rb'))
 logging.info(api_mlparams)
 
 @app.route('/infer')
-def infer(inputs):
+def infer():
     W = api_mlparams.coef_
     b = api_mlparams.intercept_
 
-    X_in=np.ndarray(shape=(1,4),buffer=np.array(inputs))
-    print("W",W)
-    print("b",b)
-    print("X_in",X_in)
-    return {'y':predict(X_in, W, b)}
+    reqX_1 = request.args.get('x_1')
+    reqX_2 = request.args.get('x_2')
+    reqX_3 = request.args.get('x_1')
+    reqX_4 = request.args.get('x_2')
+    x_1 = float(reqX_1)
+    x_2 = float(reqX_2)
+    x_3 = float(reqX_3)
+    x_4 = float(reqX_4)
+    
+    return {'y':predict(x_1,x_2,x_3,x_4,W,b)}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=False)
