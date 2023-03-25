f6abd8039256d95fa27bc25a6882fbcdad3d4e52
Gerardo Alvarez <70041405+Gerard9199@users.noreply.github.com>
2023-03-24 22:54:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-#inferir
 from flask import Flask
 from flask import request
-from sklearn.linear_model import LogisticRegression
 import pickle
 import logging
 import sys
-import numpy as np
- 
+def predict(X_in, w, b):
+    y = b + X_in[0]*w[0]+ X_in[1]*w[1] + X_in[2]*w[2] + X_in[3]*w[3]
+    return y
+
 print(__name__)
 app = Flask(__name__)
-
-filename = "mlparams"
-api_mlparams = pickle.load(open(filename, 'rb'))
+api_mlparams=pickle.load(open("mlparams", 'rb'))
 
 logging.info(api_mlparams)
 
 @app.route('/infer')
-def infer():  
-    reqX_1 = request.args.get('x_1')
-    reqX_2 = request.args.get('x_2')
-    x_1 = float(reqX_1)
-    x_2 = float(reqX_2)
-    print((x_1,type(x_1)),file=sys.stderr)
-    print((x_2,type(x_2)),file=sys.stderr)
-    X_in = np.ndarray(shape=(1,2),buffer=np.array([x_1, x_2]) )
-    return {'y':api_mlparams.predict(X_in).item()}
+def infer(inputs):
+    W = api_mlparams.coef_
+    b = api_mlparams.intercept_
+
+    X_in=np.ndarray(shape=(1,4),buffer=np.array(inputs))
+    print("W",W)
+    print("b",b)
+    print("X_in",X_in)
+    return {'y':predict(X_in, W, b)}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=False)
